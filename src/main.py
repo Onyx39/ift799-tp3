babe5083b07bd5e0e0847e93a04bc9ecb8735292
Caroline Wang <Caroline.Wang@telecomnancy.eu>
2023-12-22 02:13:28 -0500

    [+] fonctions de génération des entrées du classificateur dans des csv (non testé)

--- src/main.py
+++ src/main.py
@@ -10,6 +10,8 @@ from outils import question1 as q1
 from outils import question2 as q2
 from outils import question3 as q3
 from outils import question4 as q4
+from outils import question5 as q5
+from outils import question6 as q6
 
 if __name__ == "__main__" :
 
@@ -43,4 +45,19 @@ if __name__ == "__main__" :
 
     RATINGS1 = DataFrame(read_csv("donnees/ratings1.csv"))
 
+    # La méthode étant longue à s'exécuter, la matrice des utiliateurs est disponibles dans ce repo
     q4.creation_matrice_utilisateurs(MOVIES_MATRIX, RATINGS1, MOVIES1)
+
+    ##############
+    # QUESTION 5 #
+    ##############
+
+    PROFILS = DataFrame(read_csv("donnees/user_matrix.csv"))
+    q5.clustering_spectral(PROFILS)
+    
+    ##############
+    # QUESTION 6 #
+    ##############
+
+    ratings = read_csv('donnees/ratings1.csv')
+    q6.separer_donnees(ratings)
--- src/outils/question6.py
+++ src/outils/question6.py
@@ -3,22 +3,20 @@ Question 6
 Validation du système de recommandations
 """
 
+from heapq import merge
 import sqlite3
 from matplotlib import pyplot as plt
 import numpy as np
 import seaborn as sns
-from pandas import read_csv
+from pandas import DataFrame, read_csv
 from sklearn import metrics
 from sklearn.metrics import confusion_matrix, euclidean_distances, f1_score
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 
-def split():
+def separer_donnees(ratings):
     seed = 42
     np.random.seed(seed)
-    
-    # Charger les données
-    ratings = read_csv('donnees/ratings1.csv')
 
     # On fait une stratification des ensembles de données pour garder les même proportions
     ## Division en ensembles d'entraînement (60%) et de temp = test/validation (40%)
@@ -32,8 +30,122 @@ def split():
     ratings_evaluation.to_csv('donnees/question6/ratings_evaluation.csv', index=False)
     ratings_test.to_csv('donnees/question6/ratings_test.csv', index=False)
 
-split()
+def creation_entrees_classificateur(dataset_path, clustering_path, profils, set):
+    
+    set = read_csv(dataset_path)
+    clusters_raw = read_csv(clustering_path)
+    
+    # On ajoute une colonne 'cluster' dans le set
+    clusters_series = clusters_raw['cluster']
+    clusters = clusters_series.to_frame()
+    clusters.insert(loc=0, column='userId', value=clusters.index + 1)
+    set_clusters = set.merge(clusters, on='userId', how='left')
+    
+    data = {'userId' : [],
+            'movieId': [],
+            'userRating': [],
+            'neighborRating1': [],
+            'neighborRating2': [],
+            'neighborRating3': [],
+            'neighborRating4': [],
+            'neighborRating5': []}
+    
+    # Pour chaque utilisateur de l'ensemble set (train, validation ou test), on établit les 5 voisins
+    n = len(set_clusters)
+    for i in range(n):
+        # On récupère l'id utilisateur et film de la ligne courante
+        current_user_id = set_clusters.loc[i, 'userId']
+        current_movie_id = set_clusters.loc[i, 'movieId']
+        current_rating = set_clusters.loc[i, 'rating']
+        
+        # On sélectionne les voisins ayant le même cluster
+        neighbors_cluster_df = set_clusters[set_clusters['userId'] == current_user_id]
+        
+        # Parmi ces voisins de cluster, on sélectionne ceux qui ont noté le film voulu
+        neighbors_cluster_movie_df = neighbors_cluster_df[neighbors_cluster_df['movieId'] == current_movie_id]
+        
+        neighbors_ratings = chercher_5_plus_proches_voisins(profils, neighbors_cluster_movie_df, current_user_id)
+        
+        data['userId'].append(current_user_id)
+        data['movieId'].append(current_movie_id)
+        data['userRating'].append(current_rating)
+        
+        for index in range(5):
+            data[f'neighborRating{index}'].append(neighbors_ratings[index])
+    df = DataFrame(data)
+    df.to_csv('donnees/question6/entrees_classificateur.csv', index=False)   
+        
+def chercher_5_plus_proches_voisins(profils, cluster_neighbors, user_id):
+    closest_neighbors_ratings = [] # contiendra l'id de chaque voisin sélectionné
+    distances = [] # distances[i] contiendra les distances entre neighbors[i] et user_id
+        
+    nb_neighbors = len(cluster_neighbors)
+    for j in range(nb_neighbors):
+        current_neighbor_id = cluster_neighbors[j, 'userId']
+            
+        dist_user_i = euclidean_distances(profils[user_id-1], profils[current_neighbor_id-1])
+            
+        # S'il y a moins de 5 voisins enregistrés, on ajoute le voisin courant
+        if len(closest_neighbors_ratings) < 5:
+            closest_neighbors_ratings.append(cluster_neighbors[j, 'rating'])
+            distances.append(dist_user_i)
+        else:
+            # Sinon, on cherche le voisin enregistré le plus distant de user_id
+            dist_max = max(distances)
+            index_max = distances.index(dist_max)
+                
+            # Si le voisin courant est plus proche de user_id,
+            # on supprime le voisin enregistré le plus distant
+            # et on ajoute le voisin courant
+            if dist_user_i < dist_max:
+                    
+                _ = distances.pop(index_max)
+                _ = closest_neighbors_ratings.pop(index_max)
+                    
+                closest_neighbors_ratings.append(cluster_neighbors[j, 'rating'])
+                distances.append(dist_user_i)
+                
+    return closest_neighbors_ratings
+
+def entrainer():
+    return
+
+def chercher_hyperparametres():
+    return
+
+def predire():
+    return
+
+def evaluer():
+    return
+
+def classificateur(films, votes):
+    graine = 42
+    np.random.seed(graine)
+    
+    modele = DecisionTreeClassifier(criterion='log_loss', random_state=graine)
+    modele.fit(films, votes)
+    predictions = modele.predict(films)
 
 
+    # Affichage d'une matrice de confusion
+    
+    plt.figure()
+    confusion_mtrx = metrics.confusion_matrix(votes, predictions)
+    group_names = ['TrueNeg', 'FalsePos', 'FalseNeg','TruePos']
+    group_counts = ["{0: 0.0f}".format(value)
+    for value in confusion_mtrx.flatten()]
+    group_percentages =['{0:.2%}'.format(value) for value in confusion_mtrx.flatten() / np.sum(confusion_mtrx)]
+    labels =[f"{v1}\n{v2}\n{v3}" for v1, v2, v3 in zip(group_names, group_counts, group_percentages)]
+    labels = np.asarray(labels).reshape(2, 2)
+    ax = plt.axes()
+    sns.heatmap(confusion_mtrx, annot=labels, fmt='', cmap='Blues')
+    ax.set_title("Matrice de confusion du modèle")
+
+
+    # Affichage du score F1
+    
+    f1_res = f1_score(votes, predictions, average='weighted', labels=predictions)
+    print(f'Score F1 du modèle : {f1_res}')
 
     
